1dd1042f026e281e29431e5437d70e033f305759
musuobunengji <2480527390@qq.com>
2026-03-19 21:14:48 +0800

    feat(study-agent): add goal schema contracts

---
+++ feature_achievement/study_agent/__init__.py
@@ -0,0 +1,13 @@
+from feature_achievement.study_agent.goal_contracts import (
+    GoalConstraints,
+    GoalType,
+    LearningDepth,
+    LearningGoal,
+)
+
+__all__ = [
+    "GoalConstraints",
+    "GoalType",
+    "LearningDepth",
+    "LearningGoal",
+]
